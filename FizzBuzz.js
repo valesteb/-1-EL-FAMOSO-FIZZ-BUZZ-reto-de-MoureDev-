e96a35997f1f0827416de4c0a0db0b486e0a8ad6
Esteban Vallejo <56201388+valesteb@users.noreply.github.com>
2024-07-08 20:09:53 -0500

    Update FizzBuzz.js

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -8,12 +8,12 @@
 
 for (let i = 1; i <= 100; i++) {
   if (i % 3 === 0 && i % 5 === 0) {
-    console.log("fizzbuzz");
+    document.write("fizzbuzz");
   } else if (i % 3 === 0) {
-    console.log("fizz");
+    document.write("fizz");
   } else if (i % 5 === 0) {
-    console.log("buzz");
+    document.write("buzz");
   } else {
-    console.log(i);
+    document.write(i);
   }
 }
